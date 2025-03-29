af58c0d1136471546257500ca3ba5f4793adc3b8
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-03-29 16:04:54 +0300

    function makes the first character capital

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,13 @@
 function ucFirst(str) {
+let result="";
+
+for (let char of str) {
+  if (char == str[0]) {
+    char=str[0].toUpperCase();
+  }
+  result = result + char;
+  
+}
+return result;
   // ваш код...
 }
