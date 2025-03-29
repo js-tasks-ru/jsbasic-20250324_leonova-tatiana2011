0715a76d71394698cd7bb3bc2f509a4faf2114dc
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-03-29 16:38:52 +0300

    check spam function

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,8 +2,9 @@ function ucFirst(str) {
 let result="";
 
 for (let char of str) {
+
   if (char == str[0]) {
-    char=str[0].toUpperCase();
+    char = str[0].toUpperCase();
   }
   result = result + char;
   
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
+  str = str.toLowerCase();
+
+  if ((str.indexOf('1xbet') != -1) || (str.indexOf("xxx") != -1)) { 
+  return true;
+} else {
+  return false;
+}
   // ваш код...
 }
