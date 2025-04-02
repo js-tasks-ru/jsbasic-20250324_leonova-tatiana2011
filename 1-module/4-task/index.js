2a2b9179131d658721c53dbcaf4ff916f7a36cf4
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-04-02 22:27:59 +0300

    tasks of 2-module

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,10 +1,6 @@
 function checkSpam(str) {
   str = str.toLowerCase();
 
-  if ((str.includes('1xbet')) || (str.includes("xxx"))) { 
-  return true;
-  } else {
-  return false;
-  }
+  return str.includes('1xbet') || str.includes("xxx");
   // ваш код...
 }
--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
+  let value = 0;
+
+  for (let key in salaries) {
+
+    if ((!Number.isNaN(salaries[key])) && (Number.isFinite(salaries[key]))) {
+      value = value + salaries[key];
+    } 
+   
+  }
+   return(value);
   // ваш код...
 }
