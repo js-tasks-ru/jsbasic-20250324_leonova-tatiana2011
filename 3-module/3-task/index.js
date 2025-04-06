6037d8d6c2d735404724501cdae1b8273871df75
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-04-06 17:38:07 +0300

    3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
+  let arr = str.split(''); 
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '-') {
+      arr.splice(i,1);
+      arr[i] = arr[i].toUpperCase();
+    }
+  }
+  return arr.join('');
   // ваш код...
 }
