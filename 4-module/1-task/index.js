ab8c58104541dfa65aadca6c8237b310fde1c2e5
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-04-09 21:07:27 +0300

    4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
+  let ul = document.createElement('ul');
+  
+  friends.forEach(element => {
+      let li = document.createElement('li');
+      li.innerHTML = `${element.firstName} ${element.lastName}`;
+      ul.appendChild(li);
+  });
+  return ul;
   // ваш код...
 }
