ccc99f39e9d25ef2fed2bc7a4d82b5db61d09116
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-04-12 00:03:24 +0300

    5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
+  const button = document.querySelector('.toggle-text-button'); 
+  const text = document.getElementById('text');
+  
+  button.addEventListener('click', (event) => { 
+    text.hidden = !text.hidden;
+  // ваш код...
+});
   // ваш код...
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
+  const button_right = document.querySelector('.carousel__arrow_right');
+  const button_left = document.querySelector('.carousel__arrow_left');
+  const carousel__inner = document.querySelector('.carousel__inner');
+  const width = carousel__inner.offsetWidth;
+  const slides = document.querySelectorAll('.carousel__slide');
+  let currentIndex = 0;
+  let maxIndex = slides.length - 1;
+  button_left.style.display = 'none';
+
+  function updateButtons() {
+  if (currentIndex === 0) {
+    button_left.style.display = 'none';
+  } else {
+    button_left.style.display = '';
+  }
+
+  if (currentIndex === maxIndex) {
+    button_right.style.display = 'none';
+  } else {
+    button_right.style.display = '';
+  }
+  }
+
+  button_right.addEventListener('click', (event) => { 
+    if (currentIndex < maxIndex) {
+      currentIndex++;
+      carousel__inner.style.transform = `translateX(-${currentIndex * width}px)`;
+      updateButtons();
+    } 
+  });
+
+  button_left.addEventListener('click', (event) => { 
+    if (currentIndex > 0) {
+      currentIndex--;
+      console.log(currentIndex);
+      carousel__inner.style.transform = `translateX(-${currentIndex * width}px)`;
+      updateButtons();
+    } 
+  });
   // ваш код...
 }
