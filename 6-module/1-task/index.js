c5b7076b8d279f5d03ac857c8e60f0fbfbd1f663
Tanya Leonova <leonova.tatiana2011@yandex.ru>
2025-04-20 21:09:46 +0300

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows; 
+    
+    this.elem = document.createElement('table');
+    
+    const thead = document.createElement('thead');
+    thead.innerHTML = `
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    `;
+  this.elem.appendChild(thead);
+    const tbody = document.createElement('tbody');
+
+    for (let row of this.rows) {
+      const tr = document.createElement('tr');
+
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;
+
+      tr.querySelector('button').addEventListener('click', () => {
+        tr.remove();
+      });
+
+      tbody.appendChild(tr);
+    }
+
+    this.elem.appendChild(tbody);
   }
 }
